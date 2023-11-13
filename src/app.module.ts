8b236e800d51d6f3b697b75095df71a719fbaaf5
DongPark987 <padoho987@gmail.com>
2023-11-13 23:58:30 +0900

    chore(.env):  Hide the secret key in a .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'nestjs-database.cqtkrofxu2ek.ap-northeast-2.rds.amazonaws.com',
       port: 5432,
-      password: 'jungle2023',
-      username: 'root',
+      password: process.env.DB_USER_PASSWORD,
+      username: process.env.DB_USER_NAME,
       entities: [User],
       database: 'recre_db',
       synchronize: true,
