f6b51da580cf1d8bc4fe2545cc3dcfb0528ab501
ChoiWheatley <chltmdgus604@naver.com>
2023-11-28 20:45:29 +0900

    fix(import module): Fix dependency of modules

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from 'src/auth/auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { SessionGuard } from './session.guard';
-import { RedGreenGateway } from './game/redgreen.gateway';
-import { CatchGateway } from './game/catch.gateway';
-import { RedGreenService } from './game/redgreen.service';
+import { RedGreenGateway } from './redgreen.gateway';
+import { CatchGateway } from './catch.gateway';
+import { RedGreenService } from './redgreen.service';
+import { SessionInfoModule } from '../session-info/session-info.module';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-    imports: [JwtModule.register({}), TypeOrmModule.forFeature([User])],
-    providers: [CatchGateway, RedGreenGateway, AuthService, UserService, RedGreenService, SessionGuard],
+    imports: [JwtModule.register({}), TypeOrmModule.forFeature([User]), SessionInfoModule, UserModule, AuthModule],
+    providers: [CatchGateway, RedGreenGateway, RedGreenService, SessionGuard],
+    exports: [SessionGuard],
 })
 export class SessionModule {}
